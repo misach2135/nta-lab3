4b163535e91190a53e81518f8d38b6730ad005aa
Misach <misachenko2135@gmail.com>
2024-06-19 21:27:29 +0300

    init

---
+++ IndexSolver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class IndexSolver
+    {
+        BigInteger alpha;
+        BigInteger beta;
+        BigInteger n;
+
+        public IndexSolver(BigInteger alpha, BigInteger beta, BigInteger n)
+        {
+            this.alpha = alpha;
+            this.beta = beta;
+            this.n = n;
+        }
+
+        private List<BigInteger> formFactorBase(double c = 3.38)
+        {
+            List<BigInteger> factorBase = [];
+
+            double logN = BigInteger.Log(n);
+            double loglogN = BigInteger.Log(new BigInteger(logN));
+            double B = c * Math.Exp(0.5 * Math.Sqrt(logN * loglogN));
+
+            for (int i = 1; i < B; i++)
+            {
+
+            }
+
+
+            return [];
+        }
+
+        public BigInteger Solve()
+        {
+
+            return 0;
+        }
+
+
+    }
+}
